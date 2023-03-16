9793c7371d36db7a644ed2eff477ade0c461822e
tiskinty <tiskinty@users.noreply.github.com>
2023-03-16 14:54:52 -0400

    Update ReprocessMessages.js

--- ReprocessMessages.js
+++ ReprocessMessages.js
@@ -18,21 +18,21 @@ function reprocessMessages(channeLId, filter, replace, metadataIds) {
         metadataIds = metadataIds || null; //Optional
         var messageController = com.mirth.connect.server.controllers.MessageController.getInstance();
         try {
-                        var executor = java.util.concurrent.Executors.newFixedThreadPool(1);
-                        var task = new java.util.concurrent.Callable({
-                                call: function () {
-                                        messageController.reprocessMessages(
-                                                channeLId, //String channelId, 
-                                                filter, //MessageFilter filter, //cannot be null
-                                                replace, //boolean replace, 
-                                                metadataIds //Collection<Integer> reprocessMetaDataIds
-                                        );
-                                        return "complete";
-                                }
-                        });
-                        var future = executor.submit(task);
-                        executor.shutdown();
-                
+                var executor = java.util.concurrent.Executors.newFixedThreadPool(1);
+                var task = new java.util.concurrent.Callable({
+                        call: function () {
+                                messageController.reprocessMessages(
+                                        channeLId, //String channelId, 
+                                        filter, //MessageFilter filter, //cannot be null
+                                        replace, //boolean replace, 
+                                        metadataIds //Collection<Integer> reprocessMetaDataIds
+                                );
+                                return "complete";
+                        }
+                });
+                var future = executor.submit(task);
+                executor.shutdown();
+
         } catch (e) {
                 logger.error(e)
         }
